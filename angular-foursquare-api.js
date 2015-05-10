1b2b0c59e93b8e7b44f2b32235e30acfee9470a2
Antonio Campos <acamposruiz@gmail.com>
2015-05-10 18:25:42 +0200

    Using maps

--- angular-foursquare-api.js
+++ angular-foursquare-api.js
@@ -2,24 +2,25 @@
  * Created by Antonio on 11/04/15.
  */
 angular.module('angular-foursquare-api', ['angular-googlemaps'])
-    .constant("SECTIONS",{
+    .constant("SECTIONS", {
         "all": ['food', 'drinks', 'coffee', 'shops', 'arts', 'outdoors', 'sights', 'trending', 'specials', 'topPicks']
     })
-    .factory('venuesFromCoordinates', function($http, $q, angular_foursquare_conf) {
+    .factory('venuesFromCoordinates', function ($http, $q, angular_foursquare_conf) {
         'use strict';
         var apiVersion = '20150217';
-        var venuesFromCoordinates = function(data) {
+        var venuesFromCoordinates = function (data) {
             var deferred = $q.defer();
             $http({
                 url: 'https://api.foursquare.com/v2/venues/explore',
                 params: {
-                    client_id:  angular_foursquare_conf.client_id,
-                    client_secret:  angular_foursquare_conf.client_secret,
-                    ll: data.coordinates,
+                    client_id: angular_foursquare_conf.client_id,
+                    client_secret: angular_foursquare_conf.client_secret,
+                    //ll: data.coordinates,
+                    ll: data.lat + ',' + data.lng,
                     section: data.section,
                     venuePhotos: 1,
                     sortByDistance: 1,
-                    radius: 300,
+                    radius: 1000,
                     limit: angular_foursquare_conf.limit,
                     v: apiVersion
                 },
@@ -29,18 +30,18 @@ angular.module('angular-foursquare-api', ['angular-googlemaps'])
                     'X-Requested-With': undefined
                 })
             }).
-                success(function(data){
-                    deferred.resolve(data.response.groups[0].items);
+                success(function (data) {
+                    deferred.resolve({venues:data.response.groups[0].items, coordenates:{lat: data.lat, lng: data.lng}});
                 });
             return deferred.promise;
         };
 
         return venuesFromCoordinates;
     })
-    .factory('getVenues', function(coordinatesFromAddress, venuesFromCoordinates) {
+    .factory('getVenues', function (coordinatesFromAddress, venuesFromCoordinates) {
         'use strict';
 
-        var getVenues = function(address, section){
+        var getVenues = function (address, section) {
             return coordinatesFromAddress(address, section).then(venuesFromCoordinates);
         };
 
